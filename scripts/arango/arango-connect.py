2d57d49e4ca11c52a6b546820885fd5f06cdfd4a
Battletrout <nathanmkemp@gmail.com>
2023-01-07 23:40:37 +0900

    cleaned up arango-connect and made it import-able

--- scripts/arango/arango-connect.py
+++ scripts/arango/arango-connect.py
@@ -1,24 +1,46 @@
 from pyArango.connection import *
 
 
-conn = Connection(arangoURL='http://127.0.0.1:8529', username='wordnet_user', password='password')
+def connect_to_arangodb(arangoURL='http://127.0.0.1:8529', username='wordnet_user', password=''):
+    '''
+    Creates a connection to the ArangoDB server with specified credentials. 
+    If no credentials are specified, the defaults are used:
+    arangoURL='http://127.0.0.1:8529', username='wordnet_user', password=''
+    You will need to create a user named 'wordnet_user' with password '' in the ArangoDB server, 
+    or input existing credentials.
+    '''
+    
+    conn = Connection(arangoURL, username, password)
+    return conn
 
-if not conn.hasDatabase('test_db'):
-    conn.createDatabase(name="test_db")
-db = conn["test_db"] # all databases are loaded automatically into the connection and are accessible in this fashion
 
-if not db.hasCollection('users'):
-    db.createCollection(name="users") # all collections are also loaded automatically
-collection = db["users"]
+# Testing out the connection and functionality for creating db, collections, and documents.
+def conn_validation(conn, db_name):
+    if not conn.hasDatabase(db_name):
+        conn.createDatabase(name=db_name)
+    db = conn[db_name] # all databases are loaded automatically into the connection and are accessible in this fashion
 
-for i in range(100):
+    if not conn.hasDatabase('test_db'):
+        conn.createDatabase(name="test_db")
+    db = conn["test_db"] # all databases are loaded automatically into the connection and are accessible in this fashion
+
+    if not db.hasCollection('users'):
+        db.createCollection(name="users") # all collections are also loaded automatically
+    collection = db["users"]
+
+    for i in range(100):
+        doc = collection.createDocument()
+        doc["name"] = f"Tesla-{i}"
+        doc["number"] = i
+        doc["species"] = "human"
+        doc.save()
+
     doc = collection.createDocument()
-    doc["name"] = f"Tesla-{i}"
-    doc["number"] = i
+    doc["name"] = "Tesla-101"
+    doc["number"] = 101
     doc["species"] = "human"
-    doc.save()
 
-doc = collection.createDocument()
-doc["name"] = "Tesla-101"
-doc["number"] = 101
-doc["species"] = "human"
+if __name__ == '__main__':
+    print(connect_to_arangodb.__doc__)
+    # conn = create_connection()
+    # conn_validation(conn, 'wordnet_db')
